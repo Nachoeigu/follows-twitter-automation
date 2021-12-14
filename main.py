fafebc1f4ea12dbc02755170437ad41026955730
Nachoeigu <ignacio.eiguren@gmail.com>
2021-12-14 13:08:05 -0300

    Sending the files

---
+++ main.py
@@ -0,0 +1,8 @@
+from model import TwitterBot
+
+robot = TwitterBot()
+
+#We analyze if these figures started following someone new
+robot.analyzing_figures(usernames = ['elonmusk','leomessi','nike'])
+
+
